4c1f0639bebb44415f43b63a27f134103afedfc1
devlucascalado <devlucascalado@gmail.com>
2024-08-28 10:59:20 -0300

    atualizando meu banco

--- meu_banco.py
+++ meu_banco.py
@@ -1,3 +1,39 @@
+def depositar(valor):
+    global saldo
+    global numero_deposito 
+
+    if valor > 0.0:
+        saldo += valor
+
+        numero_deposito += 1
+    
+    mensagem_deposito = f"\nR${quantia} depositado com sucesso!"
+
+    return mensagem_deposito
+
+
+def sacar(valor):
+    global saldo
+    global numero_saque
+
+    if valor <= saldo:
+        saldo-=valor
+
+        numero_saque += 1
+
+    mensagem_saque = f"\nR${quantia} sacada com sucesso!"
+
+    return mensagem_saque
+    
+
+        
+    
+    
+
+    
+
+
+numero_deposito = 0
 saldo = 0
 limite = 500
 extrato = " "
@@ -20,16 +56,31 @@ nome_usuario = input("Para inicializar o atendimento, informe o seu nome: ")
 
 print(f"\nBem vindo ao nosso sistema, {nome_usuario}!")
 
-print(f"{nome_usuario}, agora escolha a opção que você deseja: ")
+print(f"Agora escolha a opção que você deseja: ")
 
 while True:
     opcao = input(menu).upper()
 
     if opcao == 'D':
-        print("\nDepósito selecionado")
+        print("\nDepósito selecionado\n")
+
+        quantia = float(input("Informe o valor que deseja depositar: R$"))
+
+        print(depositar(quantia))
+
+
+        
     
     elif opcao == 'S':
         print("\nSaque selecionado")
+
+        quantia = float(input("Informe a quantia que deseja sacar: R$"))
+
+        if quantia <= saldo:
+            print(sacar(quantia))
+        
+        else:
+            print("\nSaldo insuficiente")
     
     elif opcao == 'E':
         print("\nExtrato selecionado")
@@ -37,6 +88,7 @@ while True:
     elif opcao == 'Q':
         print("\nAtendimento encerrado. Obrigado por nos escolher, tenha um bom dia!")
         break
+
     else:
         print("Operação inválida")
 
